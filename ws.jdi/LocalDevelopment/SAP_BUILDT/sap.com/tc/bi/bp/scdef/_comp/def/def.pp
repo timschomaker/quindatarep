26769817657dc8dbd1fc313c2d33ea8083f923c9
timschomaker <40888656+timschomaker@users.noreply.github.com>
2018-07-11 15:40:24 +0200

    Initialization SCs and Dcs

---
+++ ws.jdi/LocalDevelopment/SAP_BUILDT/sap.com/tc/bi/bp/scdef/_comp/def/def.pp
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<public-part
+		version="1.0.2"
+		xmlns="http://xml.sap.com/2002/11/PublicPart"
+		xmlns:IDX="urn:sap.com:PublicPart:1.0"
+		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		xsi:schemaLocation="http://xml.sap.com/2002/11/PublicPart ppdef.xsd">
+	<name>def</name>
+	<purpose>infrastructure</purpose>
+	<entities>
+		<entity>
+			<name>All Java classes</name>
+			<package></package>
+			<entity-type>Java Package Tree</entity-type>
+			<entity-sub-type>Class</entity-sub-type>
+		</entity>
+		<entity>
+			<name>bp</name>
+			<entity-type>Absolute Folder Tree</entity-type>
+		</entity>
+	</entities>
+</public-part>
---
+++ ws.jdi/LocalDevelopment/SAP_BUILDT/sap.com/tc/bi/sltech/_comp/def/mm.pp
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<public-part
+		version="1.0.5"
+		xmlns="http://xml.sap.com/2002/11/PublicPart"
+		xmlns:IDX="urn:sap.com:PublicPart:1.0"
+		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		xsi:schemaLocation="http://xml.sap.com/2002/11/PublicPart ppdef.xsd">
+	<name>mm</name>
+	<purpose>infrastructure</purpose>
+	<caption>LM Metamodel</caption>
+	<description>Exposes the LM meta-model as infastructure public part for build runtime</description>
+	<entities>
+		<entity-ref>
+			<dc-ref>
+				<name>tc/lm/sl/metamodel</name>
+				<vendor>sap.com</vendor>
+			</dc-ref>
+			<pp-ref>libinf</pp-ref>
+			<entity-ref-id>a4d9abf0-bfb8-11de-b633-001999394cc8</entity-ref-id>
+		</entity-ref>
+	</entities>
+</public-part>
