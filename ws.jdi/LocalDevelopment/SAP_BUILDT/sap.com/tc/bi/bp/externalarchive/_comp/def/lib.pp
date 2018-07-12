423433d6654624f4413af35efc9bfd2d3760a281
timschomaker <40888656+timschomaker@users.noreply.github.com>
2018-07-12 09:47:57 +0200

    New DC

---
+++ ws.jdi/LocalDevelopment/SAP_BUILDT/sap.com/tc/bi/bp/externalarchive/_comp/def/lib.pp
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<public-part version="1.0.5"
+  xmlns="http://xml.sap.com/2002/11/PublicPart"
+  xmlns:IDX="urn:sap.com:PublicPart:1.0"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://xml.sap.com/2002/11/PublicPart ppdef.xsd">
+  <name>lib</name>
+  <purpose>infrastructure</purpose>
+  <!-- LOGICAL TYPE: buildplugin -->
+  <caption>Build Plugin</caption>
+  <description></description>
+</public-part>
